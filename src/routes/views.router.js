ffee58474f3ec0f746295de549b94c29fe1ca8fe
sebabrennan <brennan.seba@gmail.com>
2024-05-15 22:07:04 -0300

    seguir con form en websocket

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
+import ProductManager from "../manager/products.manager.js";
+import { __dirname } from "../paht.js";
 
 const router = Router();
+const productManager = new ProductManager(`${__dirname}/db/products.json`)
 
-router.get('/', (req, res) => {
-    res.render('home')
+router.get('/', async (req, res) => {
+    const allProducts = await productManager.getProducts();
+    res.render('home', {allProducts})
 });
+
+router.get("/realTimeProducts", (req, res) => {
+    res.render("realTimeProducts")
+})
 
 export default router;
